516112f2b85545048a60a7585cfed1386f9098f4
Ophiucus1 <etienne.pinglier@gmail.com>
2026-01-09 13:29:58 +0100

    ouverture de packs + RAAHHHHHHHHHHHHh

--- Assets/Scripts/Scene/CardUI.cs
+++ Assets/Scripts/Scene/CardUI.cs
@@ -5,12 +5,13 @@ using TMPro;
 public class CardUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI numberText;
-    [SerializeField] private Image cardImage;
+    [SerializeField] public Image cardImage;
     [SerializeField] private Image borderImage;
 
     public void SetCardData(int number, Sprite sprite, Color? borderColor = null)
     {
-        numberText.text = number.ToString();
+        if (numberText!=null)
+            numberText.text = number.ToString();
         cardImage.sprite = sprite;
 
         if (borderColor.HasValue)
